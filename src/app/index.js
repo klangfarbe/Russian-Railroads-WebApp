8af3d19bb5d6b82e5489f1f8f513a1dc319bd200
Christian Mocek <christian.mocek@gmail.com>
2016-06-11 14:02:51 +0200

    Added more logic and fixed bugs

--- src/app/index.js
+++ src/app/index.js
@@ -7,9 +7,16 @@
         $logProvider.debugEnabled(true);
     })
 
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+        .primaryPalette('lime')
+        .accentPalette('blue-grey')
+        .warnPalette('deep-orange')
+        .dark()
+        ;
+    })
+
     .run(function runBlock($log) {
         $log.debug('runBlock end');
     })
-
-    .constant('moment', moment);
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -4,55 +4,99 @@
     angular.module('rrrEmil').controller('MainController', MainController);
 
     /** @ngInject */
-    function MainController($scope, lodash) {
+    function MainController(lodash) {
+        var vm = this;
+
         var availableCards = [
-            {id: 0, cost: "2 Worker", action: "3 black tracks"},
-            {id: 1, cost: "2 Worker", action: "3 silver tracks"},
-            {id: 2, cost: "1 Worker", action: "1 brown track"},
-            {id: 3, cost: "2 Worker", action: "2 brown tracks"},
-            {id: 4, cost: "1 Worker", action: "1 natural track"},
-            {id: 5, cost: "2 Worker", action: "2 natural tracks"},
-            {id: 6, cost: "1 Worker", action: "1 white track"},
-            {id: 7, cost: "2 Worker", action: "2 white tracks"},
-            {id: 8, cost: "1 Worker and 1 Coin", action: "2 wildcard tracks"},
-            {id: 9, cost: "1 Worker", action: "Take either a locomotive or a factory"},
-            {id: 10, cost: "3 Workers", action: "Take a locomotive and a factory"},
-            {id: 11, cost: "See engineer", action: "Use the public engineer"},
-            {id: 12, cost: "1 Coin", action: "Recruit the engineer"},
-            {id: 13, cost: "1 Worker", action: "1 move on industry track"},
-            {id: 14, cost: "2 Workers", action: "2 moves on industry track"},
-            {id: 15, cost: "1 Worker", action: "Take a 2x double marker"}
+            {id: 0, worker: 2, cost: "2 Worker", action: "3 black tracks"},
+            {id: 1, worker: 2, cost: "2 Worker", action: "3 silver tracks"},
+            {id: 2, worker: 1, cost: "1 Worker", action: "1 brown track"},
+            {id: 3, worker: 2, cost: "2 Worker", action: "2 brown tracks"},
+            {id: 4, worker: 1, cost: "1 Worker", action: "1 natural track"},
+            {id: 5, worker: 2, cost: "2 Worker", action: "2 natural tracks"},
+            {id: 6, worker: 1, cost: "1 Worker", action: "1 white track"},
+            {id: 7, worker: 2, cost: "2 Worker", action: "2 white tracks"},
+            {id: 8, worker: 1, cost: "1 Worker and 1 Coin", action: "2 wildcard tracks"},
+            {id: 9, worker: 1, cost: "1 Worker", action: "Take either a locomotive or a factory"},
+            {id: 10, worker: 3, cost: "3 Workers", action: "Take a locomotive and a factory"},
+            {id: 11, worker: 1, cost: "1 Worker", action: "Use the public engineer"},
+            {id: 12, worker: 0, cost: "1 Coin", action: "Recruit the engineer"},
+            {id: 13, worker: 1, cost: "1 Worker", action: "1 move on industry track"},
+            {id: 14, worker: 2, cost: "2 Workers", action: "2 moves on industry track"},
+            {id: 15, worker: 1, cost: "1 Worker", action: "Take a 2x double marker"}
         ];
 
-        var playedCards = [
-        ];
+        var playedCards = [];
+        var occupiedCards = [];
 
-        $scope.drawCard = function() {
+        vm.baseWorkers = 6;
+        vm.fabOpen = false;
+
+        vm.drawCard = function() {
             var length = playedCards.length;
             do {
-                var id = Math.floor((Math.random() * 100) + 1) % 16;
-                if(!lodash.includes(playedCards, availableCards[id], 'id')) {
-                    playedCards.push(availableCards[id]);
+                var card = lodash.sample(availableCards);
+                if(vm.getWorkers() - card.worker >= 0 && !lodash.includes(playedCards, card, 'id')) {
+                    playedCards.push(card);
                 }
             }
-            while(playedCards.length <= 16 && playedCards.length == length)
+            while(playedCards.length <= availableCards.length && playedCards.length == length)
         };
 
-        $scope.newRound = function() {
-            playedCards.length = 0;
+        vm.redrawCard = function() {
+            if(playedCards.length > 0)
+                playedCards.length--;
+                vm.drawCard();
         }
 
-        $scope.getLast = function() {
+        vm.rejectCard = function() {
+            occupiedCards.push(lodash.last(playedCards));
+            playedCards.length--;
+        }
+
+        vm.newRound = function() {
+            playedCards.length = 0;
+            occupiedCards.length = 0;
+            vm.drawCard();
+        }
+
+        vm.getLast = function() {
             return lodash.last(playedCards);
         }
 
-        $scope.getPlayedCards = function() {
+        vm.getPlayedCards = function() {
             return lodash.slice(playedCards, 0, playedCards.length-1).reverse();
         }
 
-        $scope.noCardsLeft = function() {
-            return playedCards.length === 16;
+        vm.noCardsLeft = function() {
+            return playedCards.length === availableCards.length - occupiedCards.length || vm.getWorkers() === 0;
         }
+
+        vm.nrCards = function() {
+            return availableCards.length - playedCards.length - occupiedCards.length;
+        }
+
+        vm.getOccupiedCards = function() {
+            return occupiedCards;
+        }
+
+        vm.getWorkers = function() {
+            return vm.baseWorkers - lodash.sumBy(playedCards, 'worker');
+        }
+
+        vm.getPoints = function() {
+            if(vm.baseWorkers == 8) {
+                return "150+";
+            }
+            else if(vm.baseWorkers == 7) {
+                return "50+";
+            }
+            else if(vm.baseWorkers == 6) {
+                return "< 50";
+            }
+        }
+
+        vm.drawCard();
 
     }
 })();
