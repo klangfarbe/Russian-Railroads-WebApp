5f8e99249ce94559df1e2dec4211087ab50e0080
Christian Mocek <christian.mocek@gmail.com>
2016-06-13 01:23:50 +0200

    Added german translation

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    angular.module('rrrEmil', ['ngMaterial', 'ngLodash'])
+    angular.module('rrrEmil', ['ngMaterial', 'ngLodash', 'pascalprecht.translate'])
 
     .config(function($logProvider) {
         $logProvider.debugEnabled(true);
@@ -14,6 +14,72 @@
         .warnPalette('deep-orange')
         .dark()
         ;
+    })
+
+    .config(function($translateProvider) {
+        $translateProvider.translations('de', {
+            APP_HEADLINE:   'Emil für Russian Railroads',
+            NEW_ROUND:      'Neue Runde starten',
+            PLAYER_ACTIONS: 'Spieleraktionen',
+            EMIL_ACTIONS:   'Von Emil besetzte Aktionen',
+
+            COSTS_COINS:   '{{coins}} Münze',
+            COSTS_WORKER:   '{{worker}} Arbeiter',
+            COSTS_WORKER_COINS: '{{worker}} Arbeiter und {{coins}} Münze',
+
+            CARD_0: "3 schwarze Gleise",
+            CARD_1: "3 silberne Gleise",
+            CARD_2: "1 braunes Gleis",
+            CARD_3: "2 braune Gleise",
+            CARD_4: "1 holzfarbenes Gleis",
+            CARD_5: "2 holzfarbene Gleise",
+            CARD_6: "1 weißes Gleis",
+            CARD_7: "2 weiße Gleise",
+            CARD_8: "2 beliebige Gleise",
+            CARD_9: "Nimm eine Lokomitive oder eine Fabrik",
+            CARD_10: "Nimm eine Lokomitive und eine Fabrik",
+            CARD_11: "Benutze den Ingenieur",
+            CARD_12: "Den Ingenieur anwerben",
+            CARD_13: "1 Schritt auf der Industrieleiste",
+            CARD_14: "2 Schritte auf der Industrieleiste",
+            CARD_15: "Nimm einen x2 Marker"
+        });
+
+        $translateProvider.translations('en', {
+            APP_HEADLINE:   'Emil for Russian Railroads',
+            NEW_ROUND:      'Start new round',
+            PLAYER_ACTIONS: 'Actions used by player',
+            EMIL_ACTIONS:   'Actions used by Emil',
+
+            COSTS_COINS:   '{{coins}} coin',
+            COSTS_WORKER:   '{{worker}} worker',
+            COSTS_WORKER_COINS: '{{worker}} worker and {{coins}} coin',
+
+            CARD_0: "3 black tracks",
+            CARD_1: "3 silver tracks",
+            CARD_2: "1 brown track",
+            CARD_3: "2 brown tracks",
+            CARD_4: "1 natural track",
+            CARD_5: "2 natural tracks",
+            CARD_6: "1 white track",
+            CARD_7: "2 white tracks",
+            CARD_8: "2 wildcard tracks",
+            CARD_9: "Take either a locomotive or a factory",
+            CARD_10: "Take a locomotive and a factory",
+            CARD_11: "Use the public engineer",
+            CARD_12: "Recruit the engineer",
+            CARD_13: "1 move on industry track",
+            CARD_14: "2 moves on industry track",
+            CARD_15: "Take a 2x double marker"
+        });
+
+        $translateProvider.registerAvailableLanguageKeys(['en', 'de'], {
+             'en*': 'en',
+             'de*': 'de'
+        });
+
+        $translateProvider.determinePreferredLanguage();
+        $translateProvider.useSanitizeValueStrategy('escape');
     })
 
     .run(function runBlock($log) {
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,23 +8,42 @@
         var vm = this;
 
         var availableCards = [
-            {id: 0, worker: 2, cost: "2 Worker", action: "3 black tracks"},
-            {id: 1, worker: 2, cost: "2 Worker", action: "3 silver tracks"},
-            {id: 2, worker: 1, cost: "1 Worker", action: "1 brown track"},
-            {id: 3, worker: 2, cost: "2 Worker", action: "2 brown tracks"},
-            {id: 4, worker: 1, cost: "1 Worker", action: "1 natural track"},
-            {id: 5, worker: 2, cost: "2 Worker", action: "2 natural tracks"},
-            {id: 6, worker: 1, cost: "1 Worker", action: "1 white track"},
-            {id: 7, worker: 2, cost: "2 Worker", action: "2 white tracks"},
-            {id: 8, worker: 1, cost: "1 Worker and 1 Coin", action: "2 wildcard tracks"},
-            {id: 9, worker: 1, cost: "1 Worker", action: "Take either a locomotive or a factory"},
-            {id: 10, worker: 3, cost: "3 Workers", action: "Take a locomotive and a factory"},
-            {id: 11, worker: 1, cost: "1 Worker", action: "Use the public engineer"},
-            {id: 12, worker: 0, cost: "1 Coin", action: "Recruit the engineer"},
-            {id: 13, worker: 1, cost: "1 Worker", action: "1 move on industry track"},
-            {id: 14, worker: 2, cost: "2 Workers", action: "2 moves on industry track"},
-            {id: 15, worker: 1, cost: "1 Worker", action: "Take a 2x double marker"}
+            {id: 0, worker: 2, coins: 0},
+            {id: 1, worker: 2, coins: 0},
+            {id: 2, worker: 1, coins: 0},
+            {id: 3, worker: 2, coins: 0},
+            {id: 4, worker: 1, coins: 0},
+            {id: 5, worker: 2, coins: 0},
+            {id: 6, worker: 1, coins: 0},
+            {id: 7, worker: 2, coins: 0},
+            {id: 8, worker: 1, coins: 1},
+            {id: 9, worker: 1, coins: 0},
+            {id: 10, worker: 3, coins: 0},
+            {id: 11, worker: 1, coins: 0},
+            {id: 12, worker: 0, coins: 1},
+            {id: 13, worker: 1, coins: 0},
+            {id: 14, worker: 2, coins: 0},
+            {id: 15, worker: 1, coins: 0}
         ];
+
+        lodash.forEach(availableCards, function(item) {
+            item.action = function() {
+                return 'CARD_' + item.id;
+            }
+        })
+
+        lodash.forEach(availableCards, function(item) {
+            item.cost = function() {
+                var x = 'COSTS';
+                if(item.worker > 0) {
+                    x += '_WORKER';
+                }
+                if(item.coins > 0) {
+                    x += '_COINS';
+                }
+                return x;
+            }
+        })
 
         var playedCards = [];
         var occupiedCards = [];
