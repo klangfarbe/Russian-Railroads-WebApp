61ad645e02eef1f71a4c071a6cd6ae189f6e2e91
Christian Mocek <christian.mocek@gmail.com>
2016-06-17 17:26:07 +0200

    Added gulp task to copy flags

--- gulp/build.js
+++ gulp/build.js
@@ -94,4 +94,11 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other']);
+gulp.task('build:copy-flags', function(){
+  return gulp.src($.mainBowerFiles().concat('bower_components/flag-icon-css/flags/4x3/*'))
+    .pipe($.filter('**/*.svg'))
+    .pipe($.filter('{de,gb}.svg'))
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/flags/4x3/')));
+});
+
+gulp.task('build', ['html', 'fonts', 'other', 'build:copy-flags']);
